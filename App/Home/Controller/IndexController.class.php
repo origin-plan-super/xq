7ea3c97563301cf7207b1a38b12ccb2bfed72306
艾尔尼亚-A <1173197065@qq.com>
2018-04-16 17:36:24 +0800

    新增bug

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -22,12 +22,11 @@ class IndexController extends CommonController {
         
         //找新闻
         $where=[];
-        $where['nav_id']='咨询动态';
-        $where['super_id']='咨询动态';
+        $where['nav_id']='资讯动态';
+        $where['super_id']='资讯动态';
         $news=$model->where($where)->order('is_up desc,add_time desc')->limit(6)->select();
         $news=  toTime($news,'Y-m-d');
         $this->assign('news',$news);
-        
         
         
         //关于我们
@@ -35,6 +34,10 @@ class IndexController extends CommonController {
         $where['nav_id']='关于我们';
         $where['super_id']='关于我们';
         $about=$model->where($where)->order('add_time desc')->find();
+        
+        
+        $about['info_info']= str_replace(array("\r\n", "\r", "\n"), "</br>", $about['info_info']);
+        
         $this->assign('about',$about);
         
         //找服务项目
@@ -42,21 +45,24 @@ class IndexController extends CommonController {
         $where['nav_id']='服务项目';
         $where['super_id']='服务项目';
         $service=$model->where($where)->order('is_up desc,add_time desc')->limit(4)->select();
+        $service = toTime($service,'Y-m-d');
         $this->assign('service',$service);
         
         //找保健养生
         $where=[];
         $where['nav_id']='健康养生';
         $where['super_id']='健康养生';
-        $health=$model->where($where)->order('is_up desc,add_time desc')->limit(7)->select();
+        $health=$model->where($where)->order('is_up desc,add_time desc')->limit(4)->select();
+        $health=  toTime($health,'Y-m-d');
         $this->assign('health',$health);
         
         //找我们的优势
         $where=[];
         $where['nav_id']='我们的优势';
         $where['super_id']='我们的优势';
-        $health=$model->where($where)->order('is_up desc,add_time desc')->limit(7)->select();
-        $this->assign('women',$health);
+        $women=$model->where($where)->order('is_up desc,add_time desc')->limit(4)->select();
+        $women=  toTime($women,'Y-m-d');
+        $this->assign('women',$women);
         
         
         $model=M('nav');
